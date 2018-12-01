24fe1f2f0efab3b335a689d1c62c5d973c160b85
krzyslom <krzysztofslomczynski@gmail.com>
2018-12-01 21:03:12 +0100

    Fix command, $ requires double qm

--- app/ip/server.R
+++ app/ip/server.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 server <- function(input, output, session) {
-  id <- "basename '$(cat /proc/1/cpuset)'"
+  id <- 'basename "$(cat /proc/1/cpuset)"'
   
   output$txt <- renderPrint({
     system(id, intern = TRUE)
