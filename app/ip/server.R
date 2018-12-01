9094d3104d268f374cba2efe356023f05fd8c5b0
krzyslom <krzysztofslomczynski@gmail.com>
2018-12-01 20:54:02 +0100

    Show container id instead of ip

--- app/ip/server.R
+++ app/ip/server.R
@@ -1,8 +1,9 @@
 library(shiny)
 
 server <- function(input, output, session) {
-  ip <- "ip addr show eth0 | grep 'inet\\b' | awk '{print $2}' | cut -d/ -f1"
+  id <- "basename '$(cat /proc/1/cpuset)'"
+  
   output$txt <- renderPrint({
-    system(ip, intern = TRUE)
+    system(id, intern = TRUE)
   })
 }
